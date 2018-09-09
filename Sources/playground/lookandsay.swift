f6be4c027d513181a5d2cfa68163b0991f29dc64
Erik Little <nuclear.ace@gmail.com>
2018-09-09 10:09:01 -0400

    Add look and say

---
+++ Sources/playground/lookandsay.swift
@@ -0,0 +1,32 @@
+//
+// Created by Erik Little on 9/9/18.
+//
+
+
+// var seq = [1]
+//
+// for i in 0..<10 {
+//  print("Seq \(i): \(seq)")
+//  seq = lookAndSay(seq)
+// }
+func lookAndSay(_ seq: [Int]) -> [Int] {
+  var result = [Int]()
+  var cur = seq[0]
+  var curRunLength = 1
+
+  for i in seq.dropFirst() {
+    if cur == i {
+      curRunLength += 1
+    } else {
+      result.append(curRunLength)
+      result.append(cur)
+      curRunLength = 1
+      cur = i
+    }
+  }
+
+  result.append(curRunLength)
+  result.append(cur)
+
+  return result
+}
--- Sources/playground/main.swift
+++ Sources/playground/main.swift
@@ -1,5 +1,2 @@
 import Foundation
 
-for i in 1...5000 where isMünchhausen(i) {
-  print(i)
-}
