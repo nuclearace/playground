35ee99e38e18d8098185fddee2e5464eb246ba2c
Erik Little <nuclear.ace@gmail.com>
2018-09-08 20:27:48 -0400

    add Münchhausen

--- Sources/playground/main.swift
+++ Sources/playground/main.swift
@@ -1,8 +1,5 @@
 import Foundation
 
-let a = unchurch(add(three)(four))
-let b = unchurch(mult(three)(four))
-let c = unchurch(exp(mult(four)(church(1)))(three))
-let d = unchurch(exp(mult(three)(church(1)))(four))
-
-print(a, b, c, d)
+for i in 1...5000 where isMünchhausen(i) {
+  print(i)
+}
---
+++ Sources/playground/münchhausen.swift
@@ -0,0 +1,11 @@
+//
+// Created by Erik Little on 9/8/18.
+//
+
+import Foundation
+
+func isMünchhausen(_ n: Int) -> Bool {
+  let nums = String(n).map(String.init).compactMap(Int.init)
+
+  return Int(nums.map({ pow(Double($0), Double($0)) }).reduce(0, +)) == n
+}
