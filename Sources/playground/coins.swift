a428114b78770ab3228f7c043de093efbaf64a6e
Erik Little <nuclear.ace@gmail.com>
2018-09-12 17:32:39 -0400

    don't need to cast anymore

--- Sources/playground/coins.swift
+++ Sources/playground/coins.swift
@@ -5,7 +5,7 @@
 import BigInt
 
 func countCoins(amountCents cents: Int, coins: [Int]) -> BigInt {
-  let cycle = coins.filter({ BigInt($0) <= cents }).map({ $0 + 1 }).max()! * coins.count
+  let cycle = coins.filter({ $0 <= cents }).map({ $0 + 1 }).max()! * coins.count
   var table = [BigInt](repeating: 0, count: cycle)
 
   for x in 0..<coins.count {
