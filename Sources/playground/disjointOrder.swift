39ad83a222c6f051187e18ef6a5b6d9d7009064a
Erik Little <nuclear.ace@gmail.com>
2018-10-13 14:24:18 -0400

    simplify

--- Sources/playground/disjointOrder.swift
+++ Sources/playground/disjointOrder.swift
@@ -9,13 +9,7 @@ typealias DisjointReducer<T: Hashable> = (final: [T], replaces: [T], counts: [T:
 public func disjointOrder<T: Hashable>(m: [T], n: [T]) -> [T] {
   let replaceCounts = n.reduce(into: [T: Int](), { $0[$1, default: 0] += 1 })
   let reduced = m.reduce(into: DisjointReducer([T](), n, replaceCounts), {cur, el in
-    guard cur.counts[el, default: 0] > 0 else {
-      cur.final.append(el)
-
-      return
-    }
-
-    cur.final.append(cur.replaces.removeFirst())
+    cur.final.append(cur.counts[el, default: 0] > 0 ? cur.replaces.removeFirst() : el)
     cur.counts[el]? -= 1
   })
 
