f08e17599a067deb2f0990a550afa8c087792cc9
Erik Little <nuclear.ace@gmail.com>
2019-05-22 08:19:05 -0400

    update deps

--- Package.swift
+++ Package.swift
@@ -11,9 +11,9 @@ let package = Package(
         .library(name: "Playground", targets: ["Playground"])
     ],
     dependencies: [
-        .package(url: "https://github.com/attaswift/BigInt.git", from: "3.1.0"),
+        .package(url: "https://github.com/attaswift/BigInt.git", from: "4.0.0"),
         .package(url: "https://github.com/typelift/SwiftCheck.git", from: "0.10.0"),
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "0.12.0")
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.0.0")
     ],
     targets: [
         .target(
