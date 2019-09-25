23ba9ed722463ea9208cddff1ea1b56a263d31f9
Erik Little <nuclear.ace@gmail.com>
2019-09-24 21:22:56 -0400

    add digits of pi

--- Sources/Runner/main.swift
+++ Sources/Runner/main.swift
@@ -3,3 +3,7 @@ import CStuff
 import Foundation
 import Playground
 import Dispatch
+
+for (i, digit) in DigitsOfPi().enumerated() {
+  print("\(i + 1): \(digit)")
+}
--- Sources/playground/numbers.swift
+++ Sources/playground/numbers.swift
@@ -288,3 +288,34 @@ extension FloatingPoint {
     return other.rescaledAlmostEqual(to: self, tolerance: tolerance)
   }
 }
+
+public struct DigitsOfPi: Sequence, IteratorProtocol {
+  private var r = BigInt(0)
+  private var q = BigInt(1), t = BigInt(1), k = BigInt(1)
+  private var n = BigInt(3), l = BigInt(3)
+  private var nr = BigInt(0), nn = BigInt(0)
+
+  public init() {}
+
+  public mutating func next() -> Int? {
+    while 4 * q + r - t >= n * t {
+      nr = (2 * q + r) * l
+      nn = (q * (7 * k) + 2 + (r * l)) / (t * l)
+      q = q * k
+      t = t * l
+      l = l + 2
+      k = k + 1
+      n = nn
+      r = nr
+    }
+
+    let ret = n
+
+    nr = 10 * (r - n * t)
+    n  = ((10 * (3 * q + r)) / t) - (10 * n)
+    q  = q * 10
+    r  = nr
+
+    return Int(ret)
+  }
+}
