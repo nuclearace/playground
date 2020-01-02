aa3661535feb8524b289c4b8e86fdc1504c522df
Erik Little <nuclear.ace@gmail.com>
2020-01-02 10:24:48 -0500

    factorions

--- Sources/Runner/main.swift
+++ Sources/Runner/main.swift
@@ -4,60 +4,32 @@ import Foundation
 import Playground
 import Numerics
 
-struct MonthDay: CustomStringConvertible {
-  static let months = [
-    "January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"
-  ]
+var fact = Array(repeating: 0, count: 12)
 
-  var month: Int
-  var day: Int
+fact[0] = 1
 
-  var description: String { "\(MonthDay.months[month - 1]) \(day)" }
-
-  private func isUniqueIn(months: [MonthDay], by prop: KeyPath<MonthDay, Int>) -> Bool {
-    return months.lazy.filter({ $0[keyPath: prop] == self[keyPath: prop] }).count == 1
-  }
-
-  func monthIsUniqueIn(months: [MonthDay]) -> Bool {
-    return isUniqueIn(months: months, by: \.month)
-  }
-
-  func dayIsUniqueIn(months: [MonthDay]) -> Bool {
-    return isUniqueIn(months: months, by: \.day)
-  }
-
-  func monthWithUniqueDayIn(months: [MonthDay]) -> Bool {
-    return months.firstIndex(where: { $0.month == month && $0.dayIsUniqueIn(months: months) }) != nil
-  }
+for n in 1..<12 {
+  fact[n] = fact[n - 1] * n
 }
 
-let choices = [
-  MonthDay(month: 5, day: 15),
-  MonthDay(month: 5, day: 16),
-  MonthDay(month: 5, day: 19),
-  MonthDay(month: 6, day: 17),
-  MonthDay(month: 6, day: 18),
-  MonthDay(month: 7, day: 14),
-  MonthDay(month: 7, day: 16),
-  MonthDay(month: 8, day: 14),
-  MonthDay(month: 8, day: 15),
-  MonthDay(month: 8, day: 17)
-]
+for b in 2...12 {
+  print("The factorions for base \(b) are:")
 
-// Albert knows the month, but not the day, so he doesn't have a gimmie month
-let albertKnows = choices.filter({ !$0.monthIsUniqueIn(months: choices) })
+  for i in 1..<1500000 {
+    var sum = 0
+    var j = i
 
-// Albert also knows that Bernard doesn't know, so it can't be a gimmie day
-let bernardKnows = albertKnows.filter({ !$0.monthWithUniqueDayIn(months: albertKnows) })
+    while j > 0 {
+      sum += fact[j % b]
+      j /= b
+    }
 
-// Bernard now knows the birthday, so it must be a unique day within the remaining choices
-let bernardKnowsMore = bernardKnows.filter({ $0.dayIsUniqueIn(months: bernardKnows) })
+    if sum == i {
+      print("\(i)", terminator: " ")
+      fflush(stdout)
+    }
+  }
 
-// Albert knows the birthday now, so it must be a unique month within the remaining choices
-guard let birthday = bernardKnowsMore.filter({ $0.monthIsUniqueIn(months: bernardKnowsMore) }).first else {
-  fatalError()
+  print("\n")
 }
-
-print("Cheryl's birthday is \(birthday)")
 
