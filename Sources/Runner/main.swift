dd6d705a8df3180153e7b78514da4c1c21cb6395
Erik Little <nuclear.ace@gmail.com>
2020-01-13 07:18:59 -0500

    fib word

--- Sources/Runner/main.swift
+++ Sources/Runner/main.swift
@@ -5,12 +5,43 @@ import Foundation
 import Playground
 import Numerics
 
-func binomial<T: BinaryInteger>(_ x: (n: T, k: T)) -> T {
-  let nFac = factorial(x.n)
-  let kFac = factorial(x.k)
+struct Fib: Sequence, IteratorProtocol {
+  private var cur: String
+  private var nex: String
 
-  return nFac / (factorial(x.n - x.k) * kFac)
+  init(cur: String, nex: String) {
+    self.cur = cur
+    self.nex = nex
+  }
+
+  mutating func next() -> String? {
+    let ret = cur
+
+    cur = nex
+    nex = "\(ret)\(nex)"
+
+    return ret
+  }
 }
 
-print("binomial(\(5), \(3)) = \(binomial((5, 3)))")
-print("binomial(\(20), \(11)) = \(binomial((20, 11)))")
+func getEntropy(_ s: [Int]) -> Double {
+  var entropy = 0.0
+  var hist = Array(repeating: 0.0, count: 256)
+
+  for i in 0..<s.count {
+    hist[s[i]] += 1
+  }
+
+  for i in 0..<256 where hist[i] > 0 {
+    let rat = hist[i] / Double(s.count)
+    entropy -= rat * log2(rat)
+  }
+
+  return entropy
+}
+
+for (i, str) in Fib(cur: "1", nex: "0").prefix(37).enumerated() {
+  let ent = getEntropy(str.map({ Int($0.asciiValue!) }))
+
+  print("i: \(i) len: \(str.count) entropy: \(ent)")
+}
