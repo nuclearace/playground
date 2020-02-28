09f1e8681c19d8a9fc043e3a41a934b6612d84b2
Erik Little <nuclear.ace@gmail.com>
2020-02-28 10:09:46 -0500

    fusc seq

--- Sources/Playground/sternBrocot.swift
+++ Sources/Playground/sternBrocot.swift
@@ -14,6 +14,34 @@ public struct SternBrocot: Sequence, IteratorProtocol {
   }
 }
 
+public struct FuscSeq: Sequence, IteratorProtocol {
+  private var arr = [0, 1]
+  private var i = 0
+
+  public init() {}
+
+  public mutating func next() -> Int? {
+    defer {
+      i += 1
+    }
+
+    guard i > 1 else {
+      return arr[i]
+    }
+
+    switch i & 1 {
+    case 0:
+      arr.append(arr[i / 2])
+    case 1:
+      arr.append(arr[(i - 1) / 2] + arr[(i + 1) / 2])
+    case _:
+      fatalError()
+    }
+
+    return arr.last!
+  }
+}
+
 public func fusc<T: BinaryInteger>(n: T) -> T {
   guard n > 1 else {
     return n
--- Sources/Runner/main.swift
+++ Sources/Runner/main.swift
@@ -5,28 +5,18 @@ import Foundation
 import Playground
 import Numerics
 
-var count = 0
-var rng = MTRandom()
-var arr = Array(-100_000...100_000)
-var shuffleTime = 0.0
+let first = FuscSeq().prefix(61)
 
-while true {
-  let (_, t) = ClockTimer.time {
-    arr.shuffle(using: &rng)
+print("First 61: \(Array(first))")
+
+var max = -1
+
+for (i, n) in FuscSeq().prefix(20_000_000).enumerated() {
+  let f = String(n).count
+
+  if f > max {
+    max = f
+
+    print("New max: \(i): \(n)")
   }
-
-  shuffleTime += t.duration
-
-  let idxs = arr.equilibriumIndexes()
-
-  guard !idxs.isEmpty else {
-    count += 1
-
-    continue
-  }
-
-  print(idxs, count, "\(shuffleTime)s shuffling")
-  break
 }
-
-
