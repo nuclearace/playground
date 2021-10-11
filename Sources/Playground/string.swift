8f8f72135acd550e08a29545539bf5e1e04704a6
Erik Little <nuclear.ace@gmail.com>
2021-10-11 09:21:39 -0400

    multisplit

--- Sources/Playground/string.swift
+++ Sources/Playground/string.swift
@@ -139,3 +139,36 @@ public func isPalindrome<T: CustomStringConvertible>(_ x: T) -> Bool {
 
   return true
 }
+
+extension String {
+  public func multiSplit(on seps: [String]) -> ([Substring], [(String, (start: String.Index, end: String.Index))]) {
+    var matches = [Substring]()
+    var matched = [(String, (String.Index, String.Index))]()
+    var i = startIndex
+    var lastMatch = startIndex
+
+    main: while i != endIndex {
+      for sep in seps where self[i...].hasPrefix(sep) {
+        if i > lastMatch {
+          matches.append(self[lastMatch..<i])
+        } else {
+          matches.append("")
+        }
+
+        lastMatch = index(i, offsetBy: sep.count)
+        matched.append((sep, (i, lastMatch)))
+        i = lastMatch
+
+        continue main
+      }
+
+      i = index(i, offsetBy: 1)
+    }
+
+    if i > lastMatch {
+      matches.append(self[lastMatch..<i])
+    }
+
+    return (matches, matched)
+  }
+}
--- Sources/Runner/main.swift
+++ Sources/Runner/main.swift
@@ -9,26 +9,6 @@ import Foundation
 import Playground
 import Numerics
 
-for (divs, subs) in [([2, 3], [1]), ([2, 3], [2])] {
-  print("\nMINIMUM STEPS TO 1:")
-  print("  Possible divisors:  \(divs)")
-  print("  Possible decrements: \(subs)")
+let (matches, matchedSeps) = "a!===b=!=c".multiSplit(on: ["==", "!=", "="])
 
-  let (table, hows) = minToOne(divs: divs, subs: subs, upTo: 10)
-
-  for n in 1...10 {
-    print("    mintab(  \(n)) in {  \(table[n])} by: ", hows[n].joined(separator: ", "))
-  }
-
-  for upTo in [2_000, 50_000] {
-    print("\n    Those numbers up to \(upTo) that take the maximum, \"minimal steps down to 1\":")
-    let (table, _) = minToOne(divs: divs, subs: subs, upTo: upTo)
-    let max = table.dropFirst().max()!
-    let maxNs = table.enumerated().filter({ $0.element == max })
-
-    print(
-      "      Taking", max, "steps are the \(maxNs.count) numbers:",
-      maxNs.map({ String($0.offset) }).joined(separator: ", ")
-    )
-  }
-}
+print(matches, matchedSeps.map({ $0.0 }))
